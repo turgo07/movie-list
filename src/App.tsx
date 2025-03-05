70083c32da4f045c7958b82dd09da871b5e66960
turgo07 <taumaturgodavih2212@gmail.com>
2025-03-04 20:41:55 -0400

    m

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import SearchResults from './pages/SearchResults';
 import Footer from './components/Footer';
 
 const App: React.FC = () => {
@@ -9,7 +8,6 @@ const App: React.FC = () => {
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/search" element={<SearchResults />} />
             </Routes>
             <Footer />
         </Router>
